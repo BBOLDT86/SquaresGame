ba6e70c19940cb068306f2e2d98da6d38a4cf56d
BBOLDT86 <boldt.brad@gmail.com>
2017-01-19 11:19:03 -0200

    Update Game.sol

--- Contracts/Game.sol
+++ Contracts/Game.sol
@@ -3,3 +3,9 @@ Functions:
 
 Set Winner
 >> Recieves winning numbers from Admin HTML page and pays out winner
+
+
+
+Buy Squares: (Payable) 
+
+Recieves payment from user for each square and keeps running count of squares sold 
